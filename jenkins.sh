33b67e2c205582c750544bbfd7357ee5102c8af2
Sunil Margale <109057899+Sunilmargale@users.noreply.github.com>
2024-01-18 11:02:45 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-sudo apt update -y
+sudo yum update -y
 
-sudo apt upgrade -y 
+sudo yum upgrade -y 
 
-sudo apt install openjdk-17-jre -y
+sudo yum install java-1.8.0sudo yum remove java-1.7.0-openjdk
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
-sudo apt-get install jenkins -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum install jenkins -y
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+    
