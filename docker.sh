6636afefe7b50759c87aa9804aa707d55b76d625
Sunil Margale <109057899+Sunilmargale@users.noreply.github.com>
2024-05-28 13:03:40 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -16,3 +16,38 @@ sudo apt install docker-ce -y
 #sudo systemctl status docker
 
 sudo chmod 777 /var/run/docker.sock
+
+2) other way
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+3)sonarqube installation using docker container
+
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+4)nexus installation using docker conatiner
+
+docker run -d --name Nexus -p 8081:8081 sonatype/nexus3
+
+docker exec -it <container_id> /bin/bash
+cd sonatype-work/
+cd nexus3
+ls
+cat admin.password
+
