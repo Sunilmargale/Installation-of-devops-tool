f86c3c36173ee3cbd82013638e9fec9ce3532f60
Sunil Margale <109057899+Sunilmargale@users.noreply.github.com>
2024-10-02 22:09:07 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,30 +1,20 @@
-#!/bin/bash
-sudo apt update -y
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
-
-sudo apt update -y
-
-apt-cache policy docker-ce -y
-
-sudo apt install docker-ce -y
-
-sudo systemctl status docker
-
-sudo chmod 777 /var/run/docker.sock
-
-2) other way
-
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker ubuntu
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+
 
 # Add the repository to Apt sources:
 echo \
